b32a2e974d2997e19abbb97870ea4871ac4da513
jdavyy <132872260+jdavyy@users.noreply.github.com>
2025-11-18 14:52:20 -0800

    Configure Next.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
 
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enables Static HTML Export for GitHub Pages
+  output: 'export', 
+  
+  // IMPORTANT: Base Path for project-page hosting. Must match repo name.
+  basePath: '/jdavyy', 
+
+  // Disables server-side image optimization
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
